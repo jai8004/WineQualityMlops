f29a6eb5a1d9b786fc3636f35f67ffc8d86b2c9d
Jai Singh <jai.si8004@gmail.com>
2021-07-10 20:27:20 +0530

    dvc 1 stage config done

--- src/get_data.py
+++ src/get_data.py
@@ -12,9 +12,10 @@ def read_params(config_path):
 
 def get_data(config_path):
     config = read_params(config_path)
-    #print(config)
+   # print(config)
     data_path = config["data_source"]["s3_source"]
     df = pd.read_csv(data_path, sep=",", encoding="UTF-8" )
+   # print(df.head())
     return df
 
 
